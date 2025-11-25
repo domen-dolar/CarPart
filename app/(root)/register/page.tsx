262ad10f854b88a10e7444c0638511cf05f02ede
domen-dolar <ralodomen@gmail.com>
2025-11-25 10:26:33 +0100

    Login and Register page design

--- app/(root)/register/page.tsx
+++ app/(root)/register/page.tsx
@@ -1,29 +1,35 @@
 import Link from "next/link";
 
-const Login = () => {
+const Register = () => {
     return (
         <div className="min-h-[90vh] flex items-center justify-center relative">
             <section className="w-lg text-center relative">
                 <div className="text-3xl absolute left-4/9 -top-40">
-                    Login
+                    Register
                 </div>
                 <form className="authSection" action="">
                     <label htmlFor="username">Username</label><br />
                     <input id="username" type="text" className="authInput"/><br />
 
+                    <label htmlFor="email">E-mail</label><br />
+                    <input id="email" type="email" className="authInput"/><br />
+
                     <label htmlFor="password">Password</label><br />
                     <input id="password" type="password" className="authInput"/><br />
 
+                    <label htmlFor="password">Repeat password</label><br />
+                    <input id="password" type="password" className="authInput"/><br />
+
                     <button className="button" type="submit">
-                        Login
+                        Register
                     </button><br />
 
-                    <Link className="noAccountBtn" href="/register">
-                        Don't have an account?
+                    <Link className="noAccountBtn" href="/login">
+                        Already have an account?
                     </Link>
                 </form>
             </section>
         </div>
     )
 }
-export default Login;
+export default Register;
